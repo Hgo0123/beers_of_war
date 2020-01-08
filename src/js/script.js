6f916210551312e18aba53a4d5045e97a4dd5483
Hgo0123 <hcordillot@gmail.com>
2020-01-08 18:46:34 +0100

    collision ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
+let beer;
+
 function move() {
   let mousedownTime;
-  var beer = document.querySelector(".beer");
   var arrow = document.querySelector(".arrow");
   // calcule le temps que l'on reste appuyer sur la souris
   beer.addEventListener("mousedown", () => {
@@ -14,7 +15,7 @@ function move() {
     console.log();
   });
 
-  collision();
+  // collision();
 }
 
 function resetBeerPosition() {
@@ -22,25 +23,18 @@ function resetBeerPosition() {
   oxo.animation.setPosition(beer, { x: 0, y: 0 });
 }
 
-// detect when beer touch table
-function collision() {
-  var beer = document.querySelector(".beer");
-  var table = document.querySelector(".table");
-  var touch;
-
-  oxo.elements.onCollisionWithElement(beer, table, function() {
-    touch = true;
-    console.log("win");
-    resetBeerPosition();
-    addScorePoint();
-  });
-  if (!touch) {
-    console.log("lose");
-    resetBeerPosition();
-  }
+function ifLeaveScreen() {
+  oxo.elements.onLeaveScreen(
+    beer,
+    function() {
+      resetBeerPosition();
+    },
+    true
+  );
 }
+
 function createtable(x, y) {
-  var element = oxo.elements.createElement({
+  return oxo.elements.createElement({
     type: "div",
     class: "table",
     obstacle: true,
@@ -52,6 +46,37 @@ function createtable(x, y) {
     appendTo: ".frame"
   });
 }
+// function moveTable() {
+//   setInterval(() => {
+//     var tables = document.querySelectorAll(".table");
+//     for (i = 0; i < tables.length; i++) {
+//       oxo.animation.move(tables[i], "left", 10, true);
+//     }
+//   }, 60);
+// }
+
+function addTable() {
+  let table = createtable(100, 900);
+
+  oxo.elements.onCollisionWithElement(beer, table, function() {
+    touch = true;
+    resetBeerPosition();
+    addScorePoint();
+  });
+
+  let moveInterval = setInterval(function() {
+    oxo.animation.move(table, "left", 10, true);
+  }, 60);
+
+  oxo.elements.onLeaveScreenOnce(
+    table,
+    function() {
+      table.remove();
+      clearInterval(moveInterval);
+    },
+    true
+  );
+}
 
 // add 5 point score when beer touch table
 function addScorePoint() {
@@ -59,12 +84,16 @@ function addScorePoint() {
 }
 
 function game() {
-  move();
-  createtable(100, 70);
-  createtable(100, 300);
-  createtable(100, 760);
+  beer = document.querySelector(".beer");
 
-  arrowDegrees();
+  move();
+  //createtable(100, 500);
+  //createtable(100, 1500);
+  //createtable(100, 2000);
+  //moveTable();
+
+  ifLeaveScreen();
+  setInterval(addTable, 3000);
 }
 
 // oxo.inputs.listenKeyOnce("enter", function() {
